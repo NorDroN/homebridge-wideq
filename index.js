2f2e0d8f1720893521004f4b9371c71e21b5dbec
Andrey Klimov <aklimov@gmcs.ru>
2019-05-20 23:14:31 +0300

    Add plugin information

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 'use strict';
 
+const packageFile = require('../package.json');
+
 var Service, Characteristic;
 
 module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
 	
-  homebridge.registerPlatform('homebridge-wideq', 'WideQ', wideq);
+  homebridge.registerPlatform('homebridge-wideq', 'WideQ', WideQ);
 };
 
 function WideQ(log, config) {
 	this.log = log;
     this.config = config;
 	this.name = config["name"];
+	
+	this.log('**************************************************************');
+    this.log('WideQ v' + packageFile.version + ' by NorDroN');
+    this.log('GitHub: https://github.com/NorDroN/homebridge-wideq');
+    this.log('Email: nordron@live.ru');
+    this.log('**************************************************************');
+    this.log('use config:');
+    this.log(JSON.stringify(this.config));
+    this.log('start success...');
 }
 
 WideQ.prototype = {
