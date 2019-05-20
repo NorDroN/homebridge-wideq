c9607b7f2f6ab48603cf7ea53f1eed3032b55fdc
Andrey Klimov <aklimov@gmcs.ru>
2019-05-20 21:35:06 +0300

    Update plugin info

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 'use strict';
 
+var Service, Characteristic;
+
 module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
 	
-  homebridge.registerPlatform('homebridge-wideq', 'WideQ', wideq, true);
+  homebridge.registerPlatform('homebridge-wideq', 'WideQ', wideq);
 };
 
 function WideQ(log, config) {
-  
+	this.log = log;
+    this.config = config;
+	this.name = config["name"];
 }
 
 WideQ.prototype = {
